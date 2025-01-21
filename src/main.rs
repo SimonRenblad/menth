f830ebdf41b2e6c69aebdf3f1455a2071b6f92fc
Simon Renblad <srenblad@m-labs.hk>
2025-01-21 15:06:06 +0800

    add question generation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,74 @@
 use crossterm::{
-    event::{self, Event},
+    event::{self, KeyEvent, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use getopt::{Opt, Parser};
-use std::{io, string::String, vec::Vec};
+use std::{io::{self, Write}, string::{String, ToString}, vec::Vec};
+use rand::prelude::*;
+use std::fmt;
+
+#[derive(Copy, Clone)]
+enum Operator {
+    Plus,
+    Minus,
+    Multi,
+    Div
+}
+
+impl fmt::Display for Operator {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+           Operator::Plus => write!(f, "+"),
+           Operator::Minus => write!(f, "-"),
+           Operator::Multi => write!(f, "*"),
+           Operator::Div => write!(f, "/"),
+        }
+    }
+}
+
+struct Bound {
+    max: u32,
+    min: u32,
+    op: Operator 
+}
+
+fn operate(op: &Operator, x1: &i32, x2: &i32) -> i32 {
+    match op {
+        Operator::Plus => x1 + x2,
+        Operator::Minus => x1 - x2,
+        Operator::Multi => x1 * x2,
+        Operator::Div => x1 / x2
+    }
+}
+
+struct Question {
+    val1: i32,
+    val2: i32,
+    answer: i32,
+    op: Operator,
+}
+
+impl fmt::Display for Question {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", &self.val1, &self.op, &self.val2)
+    }
+}
+
+fn generate_question(mn: u32, mx: u32, op: Operator) -> Question {
+    // uniform sampling distribution within the bounds set
+    // let mut uniform_sampler = rand::distributions::Uniform::new_inclusive(bound.min, bound.max);
+    let mut rng = rand::thread_rng();
+    let val1 = rng.gen_range(mn..mx) as i32;
+    let val2 = rng.gen_range(mn..mx) as i32;
+    let answer = operate(&op, &val1, &val2);
+    return Question {
+        val1,
+        val2,
+        answer,
+        op
+    }
+} 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args: Vec<String> = std::env::args().collect();
@@ -29,9 +93,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     execute!(io::stdout(), EnterAlternateScreen)?;
     enable_raw_mode()?;
 
+    // generate two numbers in a range
+    //
+    // return a struct of arg, operand, arg, answer
+    // print args + operand to screen
+    // take input with enter as submit
+    // if correct move on, if wrong clear and reprint
+    let q = generate_question(0, 20, Operator::Multi);
+
+    write!(io::stdout(), "{} =", &q)?;
+    io::stdout().flush()?;
+
     loop {
-        if matches!(event::read()?, Event::Key(_)) {
-            break;
+        match event::read()? {
+            Event::Key(event) => {
+                match event.code {
+                    KeyCode::Char(c) => { write!(io::stdout(), "{}", c.to_string())?; io::stdout().flush()?;  },
+                    KeyCode::Enter => break,
+                    _ => ()
+                }
+            },            
+            _ => ()
         }
     }
 
